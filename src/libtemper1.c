355cfc773cd7d7aaba39d2ec0a4fdd12f876e2ab
lynix <lynix47@gmail.com>
2015-06-07 15:37:31 +0200

    fixed copyright and source header

--- src/libtemper1.c
+++ src/libtemper1.c
@@ -1,8 +1,7 @@
-/* Copyright 2014 Alexander Koch <lynix47@gmail.com>
+/* Copyright 2015 Alexander Koch <lynix47@gmail.com>
  *
- * This file is part of 'temper2csv'.
- *
- * 'temper2csv' is distributed under the MIT License, see LICENSE file.
+ * This file is part of 'libtemper1'. 'libtemper1' is distributed under the
+ * terms of the MIT License, see file LICENSE.
  */
 
 #include <libusb-1.0/libusb.h>
